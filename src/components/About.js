2c87c6d15ea0b47b56e3123d15c0f400ade18094
DevBrianKE <kipchumbabrian47@gmail.com>
2025-04-09 12:29:05 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Links from "./Links"; // <-- import the new component
 
-function About() {
+function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && <p>{bio}</p>} {/* Only render this if bio exists */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
